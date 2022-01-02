4bc0397900277e360411b8d318a566270a7c6023
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-01 21:47:03 -0800

    changed color scheme, font styles, added background images, changed button colors, centered footer, styled about me section

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function show() {
- 
+function showCover() {
   /* Access image by id and change
   the display property to block*/
   document.getElementById('coverLetterImg')
-          .style.display = "block";
+    .style.display = "block";
 
   document.getElementById('coverLetterBtn')
-          .style.display = "none";
+    .style.display = "none";
 }
+
+function showResume() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('coverLetterImg')
+    .style.display = "block";
+
+  document.getElementById('coverLetterBtn')
+    .style.display = "none";
+}
+
+// function modal() {
+//   if (id = 'coverLetterImg') {
+//     render coverLetterImage
+//   } else {
+//     render resumeImg
+//   }
+
+// }
