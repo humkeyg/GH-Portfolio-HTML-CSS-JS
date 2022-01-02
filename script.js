422083603e1d36678ede8a638cfc5b92a457cd50
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-02 12:49:47 -0800

    resume and resume download button working

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-function showCover() {
-  /* Access image by id and change
-  the display property to block*/
-  document.getElementById('coverLetterImg')
-    .style.display = "block";
+// Get the modal
+var modal = document.getElementById("myModal");
 
-  document.getElementById('coverLetterBtn')
-    .style.display = "none";
+// Get the button that opens the modal
+var btn = document.getElementById("resumeBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function showResume() {
-  /* Access image by id and change
-  the display property to block*/
-  document.getElementById('coverLetterImg')
-    .style.display = "block";
-
-  document.getElementById('coverLetterBtn')
-    .style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-// function modal() {
-//   if (id = 'coverLetterImg') {
-//     render coverLetterImage
-//   } else {
-//     render resumeImg
-//   }
-
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
